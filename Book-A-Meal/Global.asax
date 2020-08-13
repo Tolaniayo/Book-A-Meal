c4f49ca631cc299a18e8cd83e94c0db77245ae16
Ahmad <aabdulquadir228@gmail.com>
2020-08-13 07:36:51 +0100

    First Commit

---
+++ Book-A-Meal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Book_A_Meal.MvcApplication" Language="C#" %>
---
+++ Book-A-Meal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Book_A_Meal.Startup))]
+namespace Book_A_Meal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
